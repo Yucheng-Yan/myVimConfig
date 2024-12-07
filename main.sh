b7b61a92335aa63ea224c4c53d85380b0f0fc3fa
Yucheng-Yan <yyan7935@uni.sydney.edu.au>
2024-12-07 15:08:29 +1100

    install package manager and packages I like

--- main.sh
+++ main.sh
@@ -21,3 +21,23 @@ else
     echo "vim is detected on your system"
 fi
 
+# install pathogen
+if [ ! -d ~/.vim/autoload ]; then
+	echo "installing pathogen..."
+	mkdir -p ~/.vim/autoload ~/.vim/bundle
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+	echo "pathogen is installed successfully"
+else
+	echo "pathogen is detected on your system"
+fi
+
+# install vim plugins
+echo "installing vim plugins..."
+cd ~/.vim/bundle
+git clone https://github.com/dense-analysis/ale.git
+git clone https://github.com/jlanzarotta/bufexplorer.git
+git clone https://github.com/preservim/vim-indent-guides.git
+git clone https://github.com/pangloss/vim-javascript.git
+git clone https://github.com/tpope/vim-sensible.git
+git clone https://github.com/preservim/nerdtree.git
+git clone https://github.com/wlangstroth/vim-racket.git
